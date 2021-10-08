4ee5e42c879844ab9a1c295a6ae64732f3dbdccb
DESKTOP-G53QRMN\marka <markabdilla26@gmail.com>
2021-10-08 14:38:07 +0200

    Player movement implementation

---
+++ TopDown RPG Project/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    BoxCollider2D boxColldier;
+
+    Vector3 moveDelta;
+
+    void Start()
+    {
+       boxColldier = GetComponent<BoxCollider2D>(); 
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        moveDelta = new Vector3(x,y,0);
+
+        if(moveDelta.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        transform.Translate(moveDelta * Time.deltaTime);
+    }
+}
