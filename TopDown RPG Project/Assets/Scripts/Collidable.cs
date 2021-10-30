5a5b9143fae7b6486b6d6af37337c64902e0ef5b
DESKTOP-G53QRMN\marka <markabdilla26@gmail.com>
2021-10-30 14:53:08 +0200

    Collider refactoring

--- TopDown RPG Project/Assets/Scripts/Collidable.cs
+++ TopDown RPG Project/Assets/Scripts/Collidable.cs
@@ -23,8 +23,13 @@ public class Collidable : MonoBehaviour
             {
                 continue;
             }
-            Debug.Log(hits[i].name);
+            OnCollide(hits[i]);
             hits[i] = null;
         }
+    }
+
+    protected virtual void OnCollide(Collider2D collider)
+    {
+        Debug.Log(collider.name);
     }
 }
