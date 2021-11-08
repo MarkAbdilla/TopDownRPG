4564a652b23e30a08e67b9153502bd99cdbd3ce7
DESKTOP-G53QRMN\marka <markabdilla26@gmail.com>
2021-11-08 15:23:04 +0100

    Setup of FloatingTextManager.cs

--- TopDown RPG Project/Assets/Scripts/FloatingTextManager.cs
+++ TopDown RPG Project/Assets/Scripts/FloatingTextManager.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FloatingTextManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject textContainer;
+    public GameObject textPrefab;
+
+    private List<FloatingText> floatingTexts = new List<FloatingText>();
+
+    private void Update()
     {
-        
+        foreach(FloatingText txt in floatingTexts)
+        {
+            txt.UpdateFloatingText();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Show(string msg, int fontSize, Color color, Vector3 position, Vector3 motion, float duration)
     {
-        
+        FloatingText floatingText = GetFloatingText();
+
+        floatingText.text.text = msg;
+        floatingText.text.fontSize = fontSize;
+        floatingText.text.color = color;
+        floatingText.text.transform.position = UnityEngine.Camera.main.WorldToScreenPoint(position);
+        floatingText.motion = motion;
+        floatingText.duration = duration;
+
+        floatingText.Show();
+    }
+
+    private FloatingText GetFloatingText()
+    {
+        FloatingText txt = floatingTexts.Find(t => !t.active);
+
+        if (txt == null)
+        {
+            txt = new FloatingText();
+            txt.gameobject = Instantiate(textPrefab);
+            txt.gameobject.transform.SetParent(textContainer.transform);
+            txt.text = txt.gameobject.GetComponent<Text>();
+
+            floatingTexts.Add(txt);
+        }
+
+        return txt;
     }
 }
