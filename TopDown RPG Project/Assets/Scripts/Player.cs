539d3b9d428568071986e61dcaa4dcc593e88a02
DESKTOP-G53QRMN\marka <markabdilla26@gmail.com>
2021-12-01 14:15:41 +0100

    Creation of Mover.cs and changes to Player.cs

--- TopDown RPG Project/Assets/Scripts/Player.cs
+++ TopDown RPG Project/Assets/Scripts/Player.cs
@@ -2,54 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Fighter
+public class Player : Mover
 {
-    BoxCollider2D boxColldier;
-
-    Vector3 moveDelta;
-    RaycastHit2D hit;
-
-    void Start()
-    {
-       boxColldier = GetComponent<BoxCollider2D>(); 
-    }
-
-    void FixedUpdate()
-    {
-        GetMoveDelta();
-
-        if (GetMoveDelta().x > 0)
-        {
-            transform.localScale = Vector3.one;
-        }
-        else if (GetMoveDelta().x < 0)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-
-        hit = Physics2D.BoxCast(transform.position, boxColldier.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        
-        if (hit.collider == null)
-        {
-            transform.Translate(0, GetMoveDelta().y * Time.deltaTime, 0);
-        }
-
-        hit = Physics2D.BoxCast(transform.position, boxColldier.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        
-        if (hit.collider == null)
-        {
-            transform.Translate(GetMoveDelta().x * Time.deltaTime, 0, 0);
-        }
-
-    }
-
-    private Vector3 GetMoveDelta()
+    private void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        moveDelta = new Vector3(x, y, 0);
-
-        return moveDelta;
+        UpdateMotor(new Vector3(x, y, 0));
     }
 }
